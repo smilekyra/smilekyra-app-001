aa045fd7f6441930904b898f7f469adf7cf8589f
smilekyra <kyra@kakao.com>
2025-01-09 12:43:40 +0000

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 내가 처음 만든 앱")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
